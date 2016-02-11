473c4ebcb4f87725362bacacf4db62312868599f
Yuln Kwok <guo_yulin@126.com>
2016-02-11 23:46:02 +0800

    A New Project

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by 郭钰麟 on 16/2/10.
+//  Copyright © 2016年 Yuln. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let firstFrame = CGRect(x: 0, y: 0, width: 375, height: 667)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.yellowColor()
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 0, y: 0, width: 200, height: 200)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.redColor()
+//        firstView.addSubview(secondView)
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+}
+
